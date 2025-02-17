b963766c095ede9fed5c7a797e9eb2b537daa6bd
Mitansh Pithadia <mitanshpithadia76@gmail.com>
2025-02-17 15:22:28 +0530

    "Added Docker"

--- src/main/java/com/task/org/service/JWTService.java
+++ src/main/java/com/task/org/service/JWTService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 public class JWTService {
 
     private static String SECRET_KEY = "";
-//    private static final long jwtTokenValidityInMs = 1000; // 10 hours
+    private static final long jwtTokenValidityInMs = 1000 * 60 * 60; // 10 hours
 
     public JWTService() {
         try {
@@ -37,7 +37,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 10000))
+                .expiration(new Date(System.currentTimeMillis() + jwtTokenValidityInMs))
                 .and()
                 .signWith(getKey())
                 .compact();
